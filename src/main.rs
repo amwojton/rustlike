6988f066ba7753ecbe6d41f75acc666d8d1f6c87
Aaron Wojton <awojton@gmail.com>
2020-07-03 19:24:05 -0500

    moving around

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,36 @@ struct Tcod {
     root: Root,
 }
 
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        // Toggle fullscreen
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+
+        Key {code: Escape, ..} => return true, // Exit game
+
+        // Movement
+        Key {code: Up, ..} => *player_y -= 1,
+        Key {code: Down, ..} => *player_y += 1,
+        Key {code: Left, ..} => *player_x -= 1,
+        Key {code: Right, ..} => *player_x += 1,
+
+        _ => {}
+    }
+
+    false
+}
+
 fn main() {
     tcod::system::set_fps(LIMIT_FPS);
 
@@ -21,14 +51,23 @@ fn main() {
         .title("Rustlike")
         .init();
 
-    let mut tcod = Tcod { root };
+    let mut tcod = Tcod {root};
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
 
     // Game loop
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root
+            .put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
+
+        // Handle keys and exit game if needed
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
 }

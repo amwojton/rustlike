dec0f0018ad938ed7cd6a9113bdaae9f64ef9df2
Aaron Wojton <awojton@gmail.com>
2020-07-03 21:39:50 -0500

    part 3a: dungeon building blocks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 use tcod::colors::*;
 use tcod::console::*;
 
@@ -9,7 +11,7 @@ const SCREEN_HEIGHT: i32 = 50;
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 
-const LIMIT_FPS: i32 = 20;  // Number of times per second game loop will be executed
+const LIMIT_FPS: i32 = 20; // Number of times per second game loop will be executed
 
 const COLOR_DARK_WALL: Color = Color {r: 0, g: 0, b: 100};
 const COLOR_DARK_GROUND: Color = Color {r: 50, g: 50, b: 150};
@@ -48,6 +50,26 @@ impl Tile {
     }
 }
 
+/// A rectangle on the map, used to characterise a room.
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h
+        }
+    }
+}
+
 /// This is a generic object: player, monster, item, stairs, etc.
 /// It is always represented by a character on screen.
 #[derive(Debug)]
@@ -78,13 +100,39 @@ impl Object {
     }
 }
 
-fn make_map() -> Map {
-    // Fill map with "unblocked" tiles
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+fn create_room(room: Rect, map: &mut Map) {
+    // Go through the tiles in the rectangle and make them passable
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
 
-    // Place two pillars to test the map
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    // Horizontal tunnel. 'min()' and 'max()' are used in case 'x1 > x2'
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    // Vertical tunnel
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn make_map() -> Map {
+    // Fill map with "blocked" tiles
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // Create two rooms
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
 
     map
 }
@@ -164,8 +212,8 @@ fn main() {
 
     let mut tcod = Tcod {root, con};
 
-    // Create object representing the player
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+    // Place the player inside the first room
+    let player = Object::new(25, 23, '@', WHITE);
 
     // Create an NPC
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2 - 5, '@', YELLOW);
